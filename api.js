8d17f6be3ba402b44d88e2aa270e6a590eed6b65
hari <shariharan182003@gmail.com>
2023-10-31 13:28:34 +0530

    test file

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ const app = express();
 const user_router = require("./routes/user_route"); // Ensure the correct path to your router file
 const inventory_router = require("./routes/inventory_route");
 const devicedata_router = require("./routes/devicedata_router");
+const test_router = require("./routes/test");
 
 app.use(express.json());
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use("/api/users", user_router);
 app.use("/api/inventory", inventory_router);
 app.use("/api/device", devicedata_router);
+app.use("/api/test", test_router);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
