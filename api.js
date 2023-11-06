cfb8d0853d56046b79e6c6fb9d8853e758c72a56
hari <shariharan182003@gmail.com>
2023-11-06 18:11:34 +0530

    changes

--- api.js
+++ api.js
@@ -11,13 +11,11 @@ app.use(
 const user_router = require("./routes/user_route"); // Ensure the correct path to your router file
 const inventory_router = require("./routes/inventory_route");
 const devicedata_router = require("./routes/devicedata_router");
-// const test_router = require("./routes/test");
 
 app.use("/api/users", user_router);
 app.use("/api/inventory", inventory_router);
 app.use("/api/device", devicedata_router);
 // app.use("/api/test", test_router);
-
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
